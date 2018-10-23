de594465a764c5c15c51a0015dbc7f0699da9fbc
Weijie Xiang <weijiexiang@weijies-macbook-pro.local>
2018-10-24 00:27:41 +1100

    Initial Commit

---
+++ concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  concentration
+//
+//  Created by Weijie Xiang on 24/10/18.
+//  Copyright © 2018 Weijie Xiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
